9cf69eb1189f8bbf03930e96d03737479ad58d7d
Riva Huang <rivahuang0110@gmail.com>
2022-12-02 18:42:56 +0800

    Change firestore

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -2,29 +2,29 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: "vocab-garden.firebaseapp.com",
-//   databaseURL:
-//     "https://vocab-garden-default-rtdb.asia-southeast1.firebasedatabase.app/",
-//   projectId: "vocab-garden",
-//   storageBucket: "vocab-garden.appspot.com",
-//   messagingSenderId: "508850327143",
-//   appId: "1:508850327143:web:3a9818c3bde58bd78f4ec2",
-//   measurementId: "G-BLS4DZ4R2D",
-// };
-
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY_BACKUP,
-  authDomain: "vocab-garden-backup.firebaseapp.com",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "vocab-garden.firebaseapp.com",
   databaseURL:
-    "https://vocab-garden-backup-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "vocab-garden-backup",
-  storageBucket: "vocab-garden-backup.appspot.com",
-  messagingSenderId: "273687121515",
-  appId: "1:273687121515:web:2ea19c41729fdbd022ab2e",
-  measurementId: "G-9EB3MF0DE3",
+    "https://vocab-garden-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "vocab-garden",
+  storageBucket: "vocab-garden.appspot.com",
+  messagingSenderId: "508850327143",
+  appId: "1:508850327143:web:3a9818c3bde58bd78f4ec2",
+  measurementId: "G-BLS4DZ4R2D",
 };
+
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY_BACKUP,
+//   authDomain: "vocab-garden-backup.firebaseapp.com",
+//   databaseURL:
+//     "https://vocab-garden-backup-default-rtdb.asia-southeast1.firebasedatabase.app",
+//   projectId: "vocab-garden-backup",
+//   storageBucket: "vocab-garden-backup.appspot.com",
+//   messagingSenderId: "273687121515",
+//   appId: "1:273687121515:web:2ea19c41729fdbd022ab2e",
+//   measurementId: "G-9EB3MF0DE3",
+// };
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
